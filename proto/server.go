b75ad96a4672a8883c8ebd1bf592d78990a38387
Breezy95 <fabrivlad1995@gmail.com>
2022-07-19 18:47:38 -0400

    etcd implementation

---
+++ proto/server.go
@@ -0,0 +1,16 @@
+package proto
+
+import(
+	"log"
+	"context"
+	
+)
+
+type Server struct {
+}
+
+func (s *Server) SayHello(ctx context.Context, message *Message) (*Message, error) {
+	log.Printf("Received message body from client: %s", message.Body)
+	return &Message{Body: "Server says Hi"}, nil
+
+}
